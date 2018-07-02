7e2d1cb35d50e2ef0a6e4b93b583ff7fcfb98fe9
stefangary <stefanfgary@gmail.com>
2018-07-02 16:23:05 +0100

    started build_ariane_container

---
+++ build_ariane_container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -norc
+#===========================
+# This software is distributed under
+# the terms of the GNU GPL, v3 and
+# any later version.
+# Copyright, Stefan Gary
+#===========================
+# Build the ARIANE container
+#===========================
+
+# Copy over the container contents
+gsutil rsync -r gs://viking20/ariane_container ./
+
+# Build the container
