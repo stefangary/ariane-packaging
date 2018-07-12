3896a3b6e0b192a802eabcb4b043120d15d50160
stefangary <stefanfgary@gmail.com>
2018-07-12 15:25:06 +0100

    added automated docker install

--- setup_node.sh
+++ setup_node.sh
@@ -38,13 +38,37 @@ sudo apt-get -y install emacs
 #---------------------------------------------
 # 3) Install Docker
 #---------------------------------------------
+# a) Download key for Docker repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# b) Add Docker repo to APT sources:
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Note: $(lsb_release -cs) works in bash, but it is interpreted as a
+# variable in tcsh, so if you have to use tcsh, try:
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu trusty stable"
+# where trusty can be replaced with xenial, etc. WARNING - if you
+# upgrade Ubuntu, you will need to change this line in the repo.
+
+# c) Next, update the package database with the Docker packages:
+sudo apt-get update
+
+# d) Check which repo you are installing from
+#    (Can comment this out later once you are certain it works.)
+#    Look for https urls directly from Docker.
+apt-cache policy docker-ce
+
+# e) Finally, actually install:
+sudo apt-get install -y docker-ce
+
+# f) Check docker is running
+#    (Comment out later once you are certain it works)
+sudo systemctl status docker
 
 #---------------------------------------------
 # 4) Pull Docker repository
 #---------------------------------------------
 # Need manual command line intervention for
 # private repository.
-
-
-
-
+sudo docker login
+sudo docker pull stefanfgary/ariane
