ae0ff5a0d9f3789e11686fa68fbc668f3004d632
stefangary <stefanfgary@gmail.com>
2018-08-21 16:53:00 +0100

    streamlining postprocessing

--- run/post_process_ARIANE_run.sh
+++ run/post_process_ARIANE_run.sh
@@ -26,8 +26,9 @@
 # bdep here, so do not pursue further.
 /usr/bin/ariane2tcdf -I ariane_trajectories_qualitative.nc -J -P 
 
-# Clean up - remove original output file.
-#rm -f ariane_trajectories_qualitative.nc
+# Clean up - remove original output file
+# which is very large.
+rm -f ariane_trajectories_qualitative.nc
 
 #=========================================
 # Postprocessing step 2 - split
@@ -56,10 +57,16 @@
 #/usr/bin/tcdfhist -X -80.0 50.0 0.25 -Y 30.0 85.0 0.25 -I t.cdf
 
 # A time-sliced histogram for each Case Study
-# All steps take about 35 seconds at 0.25 degree
+# All case studies and time steps take about
+# 35 seconds at 0.25 degree
 # resolution.  Each results in a 32MB file, but
 # since the files are sparse, they each compress
 # to a few hundred K.  Run time includes compression.
+# At 0.1 degree resolution, it takes total of 111 seconds
+# and each file is 200MB, but compressed to ~1MB.
+# Choose 0.25 degree resolution because that is sufficient
+# for studying large-scale (case-study to case-study)
+# pathways.
 split_list=`ls -1 split_100??.nc`
 for split in $split_list
 do
@@ -69,8 +76,16 @@ do
 done
 
 #=========================================
-# Postprocessing step 4 - survival
+# Postprocessing step 4 - contours
+#=========================================
+# Not done during parallelization because
+# the contouring must be done with
+# superimposed histograms, and not with
+# individual histograms.
+
+#=========================================
+# Postprocessing step 5 - survival
 #=========================================
 # Default output will be alive.nc
-# NOT IMPLEMENTED YET
-#tcdfalive
+# NOT IMPLEMENTED YET AND NOT EXPENSIVE,
+# SO NOT INCLUDED IN PARALLELIZATION.
